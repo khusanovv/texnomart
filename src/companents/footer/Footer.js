3c7ad7e3ea73a8aba99a2179ac8ecec94b9c33d7
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-12-06 04:19:49 +0500

    Update Modal

--- src/companents/footer/Footer.js
+++ src/companents/footer/Footer.js
@@ -1,14 +1,41 @@
 import React from "react";
 import "./Footer.css";
+import { FooterData } from "./FooterData";
+// import FooterLogo from "./logo.png";
+import { v4 as uuidv4, v4 } from "uuid";
 
 const Footer = () => {
+  uuidv4();
   return (
-    <div className="footer">
-      <img
-        src="https://texnomart.uz/_nuxt/img/playmarket-logo-kr.24580f9.svg"
-        alt=""
-      />
-    </div>
+    <>
+      <div className="footer">
+        <div className="footer__link">
+          {FooterData.map((item, index) => {
+            return (
+              <div className="link__list">
+                
+                {item.entries.map((t, index) => {
+                  console.log(t);
+                  return (
+                    <ul key={v4}>
+                      <h4 className="list__title">{t.title}</h4>
+                      {t.links.map((itm, index) => {
+                        return (
+                          <li  className="list__link">
+                            {itm.title}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+       
+      </div>
+    </>
   );
 };
 
