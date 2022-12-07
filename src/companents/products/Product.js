e3a15679e1f77a615334107ff26200481890e23e
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-12-07 17:29:15 +0500

    Update Products

--- src/companents/products/Product.js
+++ src/companents/products/Product.js
@@ -1,22 +1,39 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Product.css";
 
 export const Product = () => {
-  const url = `http://localhost:8000/v2/allproducts`;
-  const [datas, setDatas] = useState([]);
+  //   const url = `http://fakestoreapi.com/products`;
+  //   const [datas, setDatas] = useState([]);
+  //   useEffect(() => {
+  //     fetch(url)
+  //       .then((res) => {
+  //         return res.json();
+  //       })
+  //       .then((data) => {
+  //         // console.log(data);
+  //         // setDatas(data);
+  //       });
+  //   });
+
+  const [allData, setAllData] = useState([]);
   useEffect(() => {
-    fetch(url)
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setDatas(data);
-      });
-  });
+    axios
+      .get("http://localhost:8000/v2/allproducts")
+      .then((response) => setAllData(response.data))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <>
-      <div className="product">{}</div>
+      <div className="product">
+        {allData.map((product) => (
+          <div className="product-cart">
+            <img src={product?.image[0]?.url} alt="" />
+            <h1>{product.name}</h1>
+          </div>
+        ))}
+      </div>
     </>
   );
 };
