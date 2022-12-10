1809c256b1d32cee9330aaa4846c1172c2aa9eee
Boburshoh Khusanov <110517357+khusanovv@users.noreply.github.com>
2022-12-10 16:41:13 +0500

    Update All

--- src/App.js
+++ src/App.js
@@ -1,56 +1,53 @@
-
-import './App.css';
-import Banner from './companents/banner/Banner';
-import Brendlogo from './companents/brendLogo/Brendlogo';
-import Download from './companents/download/Download';
-import Footer from './companents/footer/Footer';
-import Header from './companents/header/Header';
-import TermPayment from './companents/TermPayment/TermPayment';
+import "./App.css";
+import Banner from "./companents/banner/Banner";
+import Brendlogo from "./companents/brendLogo/Brendlogo";
+import Download from "./companents/download/Download";
+import Footer from "./companents/footer/Footer";
+import Header from "./companents/header/Header";
+import TermPayment from "./companents/TermPayment/TermPayment";
 // import { InfinitySpin  } from  'react-loader-spinner'
 import PacmanLoader from "react-spinners/PacmanLoader";
-import{useState, useEffect} from 'react'
-import Products from './companents/products/Product';
+import { useState, useEffect } from "react";
+import Products from "./companents/products/Product";
+// import Cart from "./companents/cart.js/Cart";
+// import Login from './companents/login/Login';
 // import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
-
-
 function App() {
-  const [loading, setLoading] = useState(false)
-  useEffect(() =>{
-    setLoading(true)
-    setTimeout(()=>{
-      setLoading(false)
-    },2000)
-
-    // window.onload = function(){
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(false);
+    // setTimeout(()=>{
     //   setLoading(false)
-    //   console.log("hello");
+    // },2000)
 
-    // }
-    
-  },[])
+    window.onload = function () {
+      setLoading(false);
+      console.log("hello");
+    };
+  }, []);
   return (
-    < >      
-    {
-      loading ?
-      <PacmanLoader
-        color={"#fbc100"}
-        loading={loading}
-        size={50}
-        className="lodaing"
-      />
-      :
-      <> 
-      <Header />
-      <Banner />
-      <Brendlogo />
-      <Download />
-      <TermPayment />
-      <Footer />
-      <Products />
-      </>
-    }
-      
+    <>
+      {loading ? (
+        <PacmanLoader
+          color={"#fbc100"}
+          loading={loading}
+          size={50}
+          className="lodaing"
+        />
+      ) : (
+        <>
+          {/* <Login /> */}
+          {/* <Cart /> */}
+          <Header />
+          <Banner />
+          <Brendlogo />
+          <Products />
+          <Download />
+          <TermPayment />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
--- src/companents/products/Product.js
+++ src/companents/products/Product.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import "./Product.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { v4 as uuidv4 } from "uuid";
+// import { useCart } form "react-use-cart";
 
 import "swiper/css";
 import "swiper/css/pagination";
@@ -11,8 +12,10 @@ import "swiper/css/navigation";
 
 import { Pagination, Navigation } from "swiper";
 
+const array = [];
 export const Product = () => {
   const [allData, setAllData] = useState([]);
+
   useEffect(() => {
     axios
       .get("http://localhost:8000/v2/allproducts")
@@ -20,24 +23,63 @@ export const Product = () => {
 
       .catch((err) => console.log(err));
   }, []);
-
-  const nerArr = [];
-  function phone() {
-    allData.forEach((item) => {
-      if (item.productCategory === "smartphones") {
-        nerArr.push(item);
-        console.log(nerArr);
-      }
-    });
-  }
-  phone();
   return (
     <>
+      <h3 className="product__title">Zo`r Narx</h3>
       <Swiper
+        key={uuidv4()}
         slidesPerView={6}
         spaceBetween={4}
         slidesPerGroup={3}
-        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="product"
+      >
+        {allData.filter((item) => {
+          if (item.productCategory === "smartphones") {
+            array.push(item);
+          }
+        })}
+
+        {array.map((item) => {
+          return (
+            <>
+              <SwiperSlide key={uuidv4()} className="product-cart">
+                <p className="good__price">Zo'r narx</p>
+                <img className="cart__img" src={item?.image[0]?.url} alt="" />
+                <p className="cart__title">{item.name}</p>
+                <h3> {item.price} so`m</h3>
+                <div className="cart__blok">
+                  <button className="cart__btn">Savatchaga</button>
+                  <p>
+                    <img
+                      src="	https://texnomart.uz/_nuxt/img/like-default.103ba85.svg"
+                      alt=""
+                    />
+                  </p>
+                  <p>
+                    <img
+                      src="https://texnomart.uz/_nuxt/img/compare-default.2d0981d.svg"
+                      alt=""
+                    />
+                  </p>
+                </div>
+              </SwiperSlide>
+            </>
+          );
+        })}
+      </Swiper>
+
+      <h3 className="product__title">Yangi mahsulotlar</h3>
+      <Swiper
+        key={uuidv4()}
+        slidesPerView={6}
+        spaceBetween={4}
+        slidesPerGroup={3}
+        // loop={true}
         loopFillGroupWithBlank={true}
         pagination={{
           clickable: true,
@@ -47,27 +89,28 @@ export const Product = () => {
         className="product"
       >
         {allData.map((product) => (
-          // console.log(product)
-          <SwiperSlide key={uuidv4()} className="product-cart">
-            {/* <img className="cart__img" src={nerArr.image[0].id} alt="" /> */}
-            <p className="cart__title">{nerArr.name}</p>
-            <h3> {nerArr.price} so`m</h3>
-            <div className="cart__blok">
-              <button className="cart__btn">Savatchaga</button>
-              <p>
-                <img
-                  src="	https://texnomart.uz/_nuxt/img/like-default.103ba85.svg"
-                  alt=""
-                />
-              </p>
-              <p>
-                <img
-                  src="https://texnomart.uz/_nuxt/img/compare-default.2d0981d.svg"
-                  alt=""
-                />
-              </p>
-            </div>
-          </SwiperSlide>
+          <>
+            <SwiperSlide key={uuidv4()} className="product-cart">
+              <img className="cart__img" src={product?.image[0]?.url} alt="" />
+              <p className="cart__title">{product.name}</p>
+              <h3> {product.price} so`m</h3>
+              <div className="cart__blok">
+                <button className="cart__btn">Savatchaga</button>
+                <p>
+                  <img
+                    src="	https://texnomart.uz/_nuxt/img/like-default.103ba85.svg"
+                    alt=""
+                  />
+                </p>
+                <p>
+                  <img
+                    src="https://texnomart.uz/_nuxt/img/compare-default.2d0981d.svg"
+                    alt=""
+                  />
+                </p>
+              </div>
+            </SwiperSlide>
+          </>
         ))}
       </Swiper>
     </>
